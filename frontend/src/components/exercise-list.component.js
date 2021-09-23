a84d4fce975f793e2fd000a834e308acfe8cc4f0
Andreas <andreas.windefelt@gmail.com>
2021-09-23 09:14:46 +0200

    Exercise 3

--- frontend/src/components/exercise-list.component.js
+++ frontend/src/components/exercise-list.component.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { ReactDOM } from "react-dom";
 import axios from "axios";
 import _ from 'lodash';
-import { response } from "express";
 
 
 
@@ -16,28 +15,39 @@ export default class ExercisesList extends Component {
   }
 
   componentDidMount() {
+    /*
     fetch("http://localhost:8000/api/exercises")
       .then(response => {
         return response.json();
       })
       .then((data) => {
         this.setState({
-          exercisesList: data.map(exercise => ({
+          exercisesList: data.data.map(exercise => ({
             name: exercise.name,
             description: exercise.description
           }))
         });
-        console.log(data[i])
+        console.log(data)
+      })
+      */
+     axios.get("http://localhost:8000/api/exercises")
+      .then(response => {
+        this.setState({
+          exercisesList: response.data.data
+        })
       })
   }
 
   render() {
-    return <ul>{this.renderItems()}</ul>;
-  }
-
-  renderItems() {
-    return this.state.exercisesList.map(exercise => {
-      <key={exercise.name} name={exercise.name} />;
-    });
+    return (
+      <div>
+        <h4>Exercises</h4>
+          <ul>
+            {this.state.exercisesList.map(exercise => (
+              <li>{exercise.name}</li>
+            ))}
+          </ul>
+      </div> 
+    )
   }
 }
