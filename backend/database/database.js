87b387718b2aeed873a0ab00c35f198265ec6af3
erikmmkarlsson <erik.karlsson97@outlook.com>
2021-09-23 20:29:17 +0200

    minor styling

--- backend/database/database.js
+++ backend/database/database.js
@@ -6,10 +6,10 @@ const DBSOURCE = "./backend/database/sqlite.db"
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
-      // Cannot open database
-      console.error(err.message)
-      throw err
-    }else{
+        // Cannot open database
+        console.error(err.message)
+        throw err
+    } else {
         console.log('Connected to the SQLite database.')
         db.run(`CREATE TABLE user (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -20,34 +20,34 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
             activated boolean DEFAULT false,
             CONSTRAINT email_unique UNIQUE (email)
             )`,
-        (err) => {
-            if (err) {
-                // Table already created
-                
-            }else{
-                // Table just created, creating some rows
-                var insert_user = 'INSERT INTO user (name, email, password,role) VALUES (?,?,?,?)'
-                db.run(insert_user, ["manager1","manager1@example.com",md5("admin123456"),"manager"])
-                db.run(insert_user, ["manager2","manager2@example.com",md5("user123456"),"manager"])
-                db.run(insert_user, ["Erik","Erik@example.com",md5("user123456")])
+            (err) => {
+                if (err) {
+                    // Table already created
 
-            }
-        });  
+                } else {
+                    // Table just created, creating some rows
+                    var insert_user = 'INSERT INTO user (name, email, password,role) VALUES (?,?,?,?)'
+                    db.run(insert_user, ["manager1", "manager1@example.com", md5("admin123456"), "manager"])
+                    db.run(insert_user, ["manager2", "manager2@example.com", md5("user123456"), "manager"])
+                    db.run(insert_user, ["Erik", "Erik@example.com", md5("user123456")])
+
+                }
+            });
         db.run(`CREATE TABLE exercise (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name text,
             description text
             )`,
-        (err) => {
-            if (err) {
-                // Table already created
-            }else{
-                // Table just created, creating some rows
-                var insert_exercise = 'INSERT INTO exercise (name, description) VALUES (?,?)'
-                db.run(insert_exercise, ["squats","bend your legs"])
-                db.run(insert_exercise, ["push ups","push up your body"])
-            }
-        });  
+            (err) => {
+                if (err) {
+                    // Table already created
+                } else {
+                    // Table just created, creating some rows
+                    var insert_exercise = 'INSERT INTO exercise (name, description) VALUES (?,?)'
+                    db.run(insert_exercise, ["squats", "bend your legs"])
+                    db.run(insert_exercise, ["push ups", "push up your body"])
+                }
+            });
     }
 });
 
