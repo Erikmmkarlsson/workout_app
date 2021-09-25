e4a7b6a2e78cb70d1b4f3b42d2c89e79fe7a1f18
Erikmmkarlsson <56456582+Erikmmkarlsson@users.noreply.github.com>
2021-09-25 12:44:06 +0200

    fixed imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import "./App.scss";
 
 import AppNavbar from './components/AppNavbar';
 import RegisterUser from './components/RegisterUser'
--- frontend/src/components/Login/login.jsx
+++ frontend/src/components/Login/login.jsx
@@ -1,5 +1,5 @@
 
-import './login.css';
+import './login.scss';
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
