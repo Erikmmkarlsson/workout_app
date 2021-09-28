9383a0f69ebf7e8319e1f10ac0b5b53e00c86efa
Erikmmkarlsson <56456582+Erikmmkarlsson@users.noreply.github.com>
2021-09-28 15:41:06 +0200

    parsedKey = null doesn't break anymore

--- frontend/src/components/auth/GetToken.jsx
+++ frontend/src/components/auth/GetToken.jsx
@@ -4,7 +4,10 @@
 export default function GetToken(){
     const key = localStorage.getItem("REACT_TOKEN_AUTH_KEY");
     const parsedKey = JSON.parse(key);
+    if (parsedKey != null){
     const token = parsedKey.token;
     
     return token;
+    }
+    return;
 }
